3af3b35ce05b6a861028422d6644d46f5bad7f4f
AnTuA <108426137+xAnTuA@users.noreply.github.com>
2025-05-13 09:08:26 +0200

    feature(aliases): add git.sh file

---
+++ aliases/git.sh
@@ -0,0 +1,17 @@
+# Aliases for git
+
+#init
+alias giti='git init'
+
+# add
+alias gita='git add'
+
+#commit
+alias gitc='git commit -m'
+
+# push
+alias gitp='git push'
+alias gitpo='git push origin'
+
+# fetch
+alias gitfo='git fetch origin'
